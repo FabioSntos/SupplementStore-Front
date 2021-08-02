d304fdc71f456918f81062e87f710f6ca412b452
FabioSntos <57735438+FabioSntos@users.noreply.github.com>
2021-08-02 15:49:15 -0300

    feat: globalstyles

--- src/index.js
+++ src/index.js
@@ -2,10 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import { Router } from "./routes/routes";
+import { GlobalStyles } from "./styles/GlobalStyles";
 
 ReactDOM.render(
   <React.StrictMode>
-    teste
+    <GlobalStyles />
     <Router />
   </React.StrictMode>,
   document.getElementById("root")
